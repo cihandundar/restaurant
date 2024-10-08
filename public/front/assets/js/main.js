6c39a0e58bc1b10b45ce5e3005fcfed083722219
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-10-08 22:18:34 +0300

    hero sayfası düzenlendi

--- public/front/assets/js/main.js
+++ public/front/assets/js/main.js
@@ -1,5 +1,7 @@
 var swiper = new Swiper(".heroSlider", {
+    direction: "vertical",
     pagination: {
         el: ".swiper-pagination",
+        clickable: true,
     },
 });
--- resources/views/front/partials/home_section/hero.blade.php
+++ resources/views/front/partials/home_section/hero.blade.php
@@ -1,14 +1,9 @@
-@extends('front.base')
-
-
-@section('content')
-    <section class="hero-slider">
-        <div class="swiper heroSlider>
-            <div class="swiper-wrapper">
+<section class="hero-slider">
+    <div class="swiper heroSlider">
+        <div class="swiper-wrapper">
             <div class="swiper-slide"><img src="{{ asset('front/assets/images/hero1.jpg') }}" alt="heroImage"></div>
             <div class="swiper-slide"><img src="{{ asset('front/assets/images/hero2.jpg') }}" alt="heroImage"></div>
         </div>
         <div class="swiper-pagination"></div>
-        </div>
-    </section>
-@endsection
+    </div>
+</section>
