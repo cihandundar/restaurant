df00c4c840aa77e83c0e758d985029f6d133dd25
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-10-12 22:40:02 +0300

    fix

--- resources/views/front/partials/home_section/hero.blade.php
+++ resources/views/front/partials/home_section/hero.blade.php
@@ -22,10 +22,10 @@
                         <span>Delicious & Mouth</span>
                         <span>Watering Test</span>
                     </div>
-                    <div class="hero-info">
+                    <div class="reservation">
                         Best food made by our <a href="#" title="#">Passionate Chefs</a>
                     </div>
-                    <a href="#" title="#" class="hero-btn">Book a Table</a>
+                    <a href="#" title="#" class="btn">Book a Table</a>
                 </div>
             </div>
         </div>
