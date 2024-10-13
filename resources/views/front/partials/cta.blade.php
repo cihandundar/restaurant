13a21f73a3761c0dbb305cf38d976a5b7184a0ab
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-10-13 22:32:09 +0300

    new section added

---
+++ resources/views/front/partials/cta.blade.php
@@ -0,0 +1,23 @@
+<section class="cta">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-12">
+                <img src="{{ asset('front/assets/images/the-best-food.jpg') }}" alt="ctaImage">
+            </div>
+            <div class="col-lg-6">
+                <div class="cta-right">
+                    <div class="info">Newsletter</div>
+                    <div class="title">Subscribe our Newsletter</div>
+                    <div class="cta-right-text">To get latest updates, offers and promotions</div>
+                    <div class="cta-right-input">
+                        <input type="email" placeholder="Enter your email">
+                        <button>Subscribe</button>
+                    </div>
+                    <div class="reservation">
+                        Call for Reservation <a href="#">+01234 567 891</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
