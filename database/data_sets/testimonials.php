af2319dbf7d97d18b2b4b00e65bd68b77ef5ae6b
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-10-13 19:41:33 +0300

    fix

--- database/data_sets/testimonials.php
+++ database/data_sets/testimonials.php
@@ -25,28 +25,4 @@ return [
         'image' => asset('front/assets/images/reviews2.png'),
                 'job' => 'CTO, Morlex Group'
     ],
-    [
-        'name' => 'Maichel Williamson',
-        'comment' => 'Rosoi is one of the most popular and tasty Restaurant with unique & special test food menu made by our passionate more an chefs with love and quality and love',
-        'image' => asset('front/assets/images/reviews1.png'),
-                'job' => 'CTO, Morlex Group'
-    ],
-    [
-        'name' => 'Maichel Williamson',
-        'comment' => 'Rosoi is one of the most popular and tasty Restaurant with unique & special test food menu made by our passionate more an chefs with love and quality and love',
-        'image' => asset('front/assets/images/reviews2.png'),
-                'job' => 'CTO, Morlex Group'
-    ],
-    [
-        'name' => 'Maichel Williamson',
-        'comment' => 'Rosoi is one of the most popular and tasty Restaurant with unique & special test food menu made by our passionate more an chefs with love and quality and love',
-        'image' => asset('front/assets/images/reviews1.png'),
-                'job' => 'CTO, Morlex Group'
-    ],
-    [
-        'name' => 'Maichel Williamson',
-        'comment' => 'Rosoi is one of the most popular and tasty Restaurant with unique & special test food menu made by our passionate more an chefs with love and quality and love',
-        'image' => asset('front/assets/images/reviews2.png'),
-                'job' => 'CTO, Morlex Group'
-    ],
 ];
