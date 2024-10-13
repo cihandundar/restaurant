c3b82d5d7861a94c6800989ca203452208d62b71
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-10-13 19:38:13 +0300

    fix

--- public/front/assets/js/main.js
+++ public/front/assets/js/main.js
@@ -53,4 +53,20 @@ var swiper = new Swiper(".testimonialsSlider", {
         el: ".swiper-pagination",
         clickable: true,
     },
+    breakpoints: {
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+    },
 });
