23e05f861826fae31fb2c069026109611c7a1238
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-10-15 22:13:52 +0300

    footer section added

--- resources/views/front/partials/footer.blade.php
+++ resources/views/front/partials/footer.blade.php
@@ -1,1 +1,48 @@
-Footer
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-12">
+                <a href="/" class="footer-logo">
+                    @include('front.partials.svg.logo')
+                </a>
+                <div class="footer-text">
+                    Rosoi is one of the most popular Restaurant & special menu made by our passionate chefs
+                </div>
+                <div class="social-icons">
+                    <a href="#" title="#" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#" title="#" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#" title="#" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#" title="#" target="_blank"><i class="fa-brands fa-whatsapp"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-9 col-12">
+                <div class="footer-list">
+                    <ul>
+                        <div class="list-title">Restaurant</div>
+                        <li><a href="#" title="#">About us</a></li>
+                        <li><a href="#" title="#">Our Menu</a></li>
+                        <li><a href="#" title="#">Book a Table</a></li>
+                        <li><a href="#" title="#">Blog Post</a></li>
+                        <li><a href="#" title="#">Contact</a></li>
+                    </ul>
+                    <ul>
+                        <div class="list-title">Information</div>
+                        <li><a href="#" title="#">Terms & Conditions</a></li>
+                        <li><a href="#" title="#">24/7 Service</a></li>
+                        <li><a href="#" title="#">Style Guide</a></li>
+                        <li><a href="#" title="#">Licenses</a></li>
+                        <li><a href="#" title="#">Change Log</a></li>
+                    </ul>
+                    <ul>
+                        <div class="list-title">Contact info</div>
+                        <li><a href="#" title="#">245 D, South City, Mixed Tower Main town, New York, USA</a>
+                        </li>
+                        <li><a href="mailto:info@rosoi.com">info@rosoi.com</a></li>
+                        <li><a href="mailto:admin@examplesite.com">admin@examplesite.com</a></li>
+                        <li><a href="#" title="#">+01234 567 891</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
