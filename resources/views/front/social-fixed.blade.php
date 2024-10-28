32fe0cf6e8cc4cf33e8a81fdedabc5689d7de0f0
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-10-28 22:49:55 +0300

    fix

---
+++ resources/views/front/social-fixed.blade.php
@@ -0,0 +1,17 @@
+<div id="fixed-social">
+    <div>
+        <a href="#" class="fixed-facebook" target="_blank"><i class="fa-brands fa-facebook-f"></i>
+            <span>Facebook</span></a>
+    </div>
+    <div>
+        <a href="#" class="fixed-twitter" target="_blank"><i class="fa-brands fa-twitter"></i><span>Twitter</span></a>
+    </div>
+    <div>
+        <a href="#" class="fixed-gplus" target="_blank"><i
+                class="fa-brands fa-whatsapp"></i><span>Whatsapp</span></a>
+    </div>
+    <div>
+        <a href="#" class="fixed-instagrem" target="_blank"><i class="fa-brands fa-instagram"></i>
+            <span>Instagram</span></a>
+    </div>
+</div>
